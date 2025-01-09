8aa4064f38826d2164b470e2120b74801c5a890c
Yohai Wiener <yohaiwiener@gmail.com>
2025-01-09 22:49:28 +0200

    adding suspense to search abr

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,12 @@
 import { SearchBar } from '@/components/forms/SearchBar'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col">
-      <SearchBar />
+      <Suspense fallback={null}>
+        <SearchBar />
+      </Suspense>
       {children}
     </div>
   )
